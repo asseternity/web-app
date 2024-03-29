3b82df84e5d06c7a00aa817dd4644edefd2374e7
asseternity <nakupovasset@gmail.com>
2024-03-29 12:47:33 +0500

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,32 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    index: path.resolve(__dirname, "src/index.js"),
+  },
+  watchOptions: {
+    ignored: /node_modules/,
+    aggregateTimeout: 300,
+    poll: 1000,
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|mp3)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
